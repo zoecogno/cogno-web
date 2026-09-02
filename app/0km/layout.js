9cf6ba9d458b31d9b6068c80bbf4986d81577844
zoecogno <cognozoe@gmail.com>
2026-09-02 09:49:55 -0300

    Add layout.js for 0 KM vehicle metadata and layout

---
+++ app/0km/layout.js
@@ -0,0 +1,24 @@
+export const metadata = {
+  title: 'Vehículos 0 KM Multimarca | Cogno Automotores',
+  description: 'Conocé nuestra gama de 0 KM de todas las marcas con entrega inmediata, la mejor toma de usados y financiación en Río Cuarto.',
+  openGraph: {
+    title: 'Vehículos 0 KM Multimarca | Cogno Automotores',
+    description: 'Encontrá tu próximo 0 KM de todas las marcas con entrega inmediata y asesoramiento personalizado.',
+    url: 'https://www.cognoautomotores.com.ar/0km',
+    siteName: 'Cogno Automotores',
+    images: [
+      {
+        url: 'https://www.cognoautomotores.com.ar/logo-black.png',
+        width: 1200,
+        height: 630,
+        alt: '0 KM Multimarca - Cogno Automotores',
+      },
+    ],
+    locale: 'es_AR',
+    type: 'website',
+  },
+};
+
+export default function CeroKmLayout({ children }) {
+  return children;
+}
