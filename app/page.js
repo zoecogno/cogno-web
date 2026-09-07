a8786d82529db60ac438ca0034ec4203f1e9908a
zoecogno <cognozoe@gmail.com>
2026-09-07 15:49:15 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -692,7 +692,7 @@ export default function Home() {
       <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
       <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
     </svg>
-    <span>Seguinos en instagram <strong>@cognoautomotores</strong></span>
+    <span>Seguinos en instagram</span>
   </a>
 </div>
     </div>
